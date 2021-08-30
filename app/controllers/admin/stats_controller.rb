f56563cd6e92ec31e2fb83634cc04e3e9800cb63
zdurr22 <zdurr22@gmail.com>
2021-08-30 14:11:52 -0400

    Done.

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -1,8 +1,9 @@
-class StatsController < ApplicationController
+class Admin::StatsController < ApplicationController
 
   def index
     @post_count = Post.count
     @authors_count = Author.count
     @last_post = Post.last
   end
+
 end
